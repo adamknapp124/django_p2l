84f70096eb95712da66e502947472ad6cdabf387
Adam Knapp <aknapp124@gmail.com>
2022-10-06 16:34:20 -0700

    record score addition

--- games/views.py
+++ games/views.py
@@ -1,5 +1,26 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from .models import GameScore
+
+
+def record_score(request):
+    data = json.loads(request.body)
+
+    user_name = data['user-name']
+    game = data['game']
+    score = data['score']
+
+    new_score = GameScore(user_name=user_name, game=game, score=score)
+    new_score.save()
+
+    response = {
+        'success': True
+    }
+
+    return JsonResponse(response)
 
 # Create your views here.
 class HomeView(TemplateView):
