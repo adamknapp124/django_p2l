1d73fb028afa0696145a45a733e37304e22d5c46
Adam Knapp <aknapp124@gmail.com>
2022-10-09 14:51:07 -0700

    games model

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Game
+
 # Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    model = Game
+    list_display = ['game_name', 'high_score', 'last_played']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('game_name', 'high_score', 'last_played')
+
+        return()
--- games/models.py
+++ games/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.urls import reverse
+
+from common.utils.text import unique_slug
 
 # Create your models here.
 class GameScore(models.Model):
@@ -15,3 +18,24 @@ class GameScore(models.Model):
     game = models.TextField(choices=GAME_CHOICES, default=MATH)
     score = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
+
+class Game(models.Model):
+    game_name = models.TextField(max_length=200)
+    high_score = models.IntegerField()
+    last_played = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=True, editable=False
+    )
+
+    def get_absolute_url(self):
+        return reverse('games:detail', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.game_name
