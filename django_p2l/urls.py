3bc5b6d0c87352313ce12ab09e7feb1b17661e6c
Adam Knapp <aknapp124@gmail.com>
2022-10-11 15:18:05 -0700

    fixed admin docs not having a link

--- django_p2l/urls.py
+++ django_p2l/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # Admin
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 
     # User Management
