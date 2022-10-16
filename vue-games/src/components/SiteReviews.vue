7f8980c2d789ef3d1e3fe64b534924fa3fb4a53a
Adam Knapp <aknapp124@gmail.com>
2022-10-15 17:29:06 -0700

    contact form now sends emails and goodbye HelloWorld

---
+++ vue-games/src/components/SiteReviews.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="row mx-1 my-3">
+		<label :for="id" class="col font-weight-bold">{{ label }}</label>
+		<input
+			:id="id"
+			:value="currentValue"
+			@input="$emit('input', $event.target.value)"
+			class="col"
+		/>
+			<option
+				v-for="option in options"
+				:key="option[1]"
+				:value="option[1]"
+			>
+				{{ option[0] }}
+			</option>
+	</div>
+</template>
+
+<script>
+
+</script>
