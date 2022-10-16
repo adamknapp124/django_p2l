986a391649622ade59c123f8d31811e1f6b179f3
Adam Knapp <aknapp124@gmail.com>
2022-10-15 17:53:12 -0700

    Added site reviews link to nav

---
+++ vue-games/src/apps/SiteReviews.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: 'SiteReviews',
+    data() {
+        return {
+            author: '',
+            review: '',
+        }
+    }
+}
+</script>
